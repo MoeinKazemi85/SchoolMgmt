d9cab39a16082dc6aff3cd56f456d17c1e8b358c
MoeinKazemi85 <mohammadmoeinkazemi1385@gmail.com>
2024-11-30 10:17:06 +0330

    ایجاد رفرنس و افزودن EF-Nuget

---
+++ BLL/Logic/StudentLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Logic
+{
+    public class StudentLogic
+    {
+        //CRUD (Create,Update,Select,Delete)
+
+    }
+}
